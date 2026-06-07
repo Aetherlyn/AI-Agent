7474892e88891c58cdacc05bcc2884823f5fb3bc
Aetherlyn <ahmetaydincapar@gmail.com>
2026-06-07 14:43:15 +0300

    CH2L3

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,6 +3,13 @@ import os
 
 def get_files_info(working_directory: str, directory: str = ".") -> str:
     try:
+       
+        if directory == ".":
+            directory_name = "current"
+        else:
+            directory_name = f"'{directory}'"
+
+
         absolute_working_directory = os.path.abspath(working_directory)
 
         target_directory = os.path.normpath(os.path.join(absolute_working_directory, directory))
@@ -13,9 +20,27 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         if not valid_target_directory:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         
-        if not os.path.isdir(directory):
+        if not os.path.isdir(target_directory):
             return f'Error: "{directory}" is not a directory'
-        
-        return f'Success: "{directory}" is within the working directory'
+
+        directory_content_list = os.listdir(target_directory)
+
+        result_list = []
+        for file in directory_content_list:
+            item_path = os.path.join(target_directory, file)
+            if os.path.isdir(item_path):
+                is_path = "is_dir=True"
+            else:
+                is_path = "is_dir=False"
+            
+            file_size = f"file_size={os.path.getsize(item_path)} bytes"
+
+            result = f" - {file}: {file_size}, {is_path}"
+            
+            result_list.append(result)
+
+        return '\n'.join(result_list)           
+
+
     except Exception as e:
         return f"Error: {e}"
